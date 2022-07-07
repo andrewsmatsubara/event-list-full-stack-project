07d654d5b9ecf0be89fe59826eec9267a5886559
Andrews Matsubara <andrewsmatsubara@gmail.com>
2022-07-07 20:11:33 -0300

    Created Registration page and its route, wrapped a Link with Button component and  implemented handleChange function with hooks for states inside Login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router';
 import { Home } from './pages/Home';
 import { Login } from './pages/Login';
+import { Registration } from './pages/Registration';
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Routes>
         <Route exact path='/' element={<Login />} />
         <Route exact path='/home' element={<Home />} />
+        <Route exact path='/registration' element={<Registration />} />
       </Routes>
     </div>
   );
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,15 +1,33 @@
 import { Button } from '../components/Button'
 import { Input } from '../components/Input'
 import '../style/Login.css'
+import { Link } from 'react-router-dom'
+import { useState } from 'react'
 
 export const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleChange = (e) => {
+    if (e.target.placeholder === 'email') {
+      setEmail(e.target.value)
+    } else if (e.target.placeholder === 'password') {
+      setPassword(e.target.value);
+    }
+  }
+
   return (
     <div className='login-page'>
       <form className='login-form'>
-        <Input type='text' placeholder='email'
-         />
-        <Input type='password' placeholder='password' />
-        <Button>Acessar</Button>
+        <h1>Login</h1>
+        <Input type='text' placeholder='email' onChange={(e) => handleChange(e)} />
+        <Input type='password' placeholder='password' onChange={(e) => handleChange(e)} />
+        <Button>
+          <Link to={(email === '' || password === '') ? '/' : '/home'}>
+            Acessar
+          </Link>
+        </Button>
+        <Link to='/registration'>Registrar</Link>
       </form>
     </div>
   )
