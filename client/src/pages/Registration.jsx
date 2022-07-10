de070af0e8625b7a5a5af45993ca6d904f8a6e65
Andrews Matsubara <andrewsmatsubara@gmail.com>
2022-07-10 02:08:34 -0300

    Basic registration page

--- client/src/pages/Registration.jsx
+++ client/src/pages/Registration.jsx
@@ -1,5 +1,27 @@
+import { Input } from '../components/Input'
+import { Button } from '../components/Button'
+import { useState } from 'react';
+
+const fetch = require('node-fetch');
+
 export const Registration = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleChange = (e) => {
+    if (e.target.placeholder === 'email') {
+      setEmail(e.target.value)
+    } else if (e.target.placeholder === 'password') {
+      setPassword(e.target.value);
+    }
+  }
+
   return (
-    <div>Registration</div>
+    <section>
+      <div>Registrar conta</div>
+      <Input type="text" placeholder="email" onChange={(e) => handleChange(e)} />
+      <Input type="password" placeholder="password" onChange={(e) => handleChange(e)} />
+      <Button>Registrar</Button>
+    </section>
   );
 }
