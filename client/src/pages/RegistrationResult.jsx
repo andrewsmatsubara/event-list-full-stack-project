acb34d4da824c4b20a00e0f7683360cd701e190f
Andrews Matsubara <andrewsmatsubara@gmail.com>
2022-07-11 20:56:09 -0300

    Centralized login and registration forms

--- client/src/pages/RegistrationResult.jsx
+++ client/src/pages/RegistrationResult.jsx
@@ -1,12 +1,13 @@
 import { RegistrationFailed } from '../components/RegistrationFailed';
 import { RegistrationSuccess } from '../components/RegistrationSuccess';
 import { store } from '../redux/store';
+import '../style/RegistrationResult.css';
 
 export const RegistrationResult = () => {
   const state = store.getState().reducers.state.newValue;
 
   return (
-    <div>
+    <div className='registration-result-page'>
       {state ? <RegistrationFailed /> : <RegistrationSuccess />}
     </div>
   )
